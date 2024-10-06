087fef43ab53936586a86e8ce30f55e148bb0cea
rusgyn <rusgynmagsanay@gmail.com>
2024-10-06 07:50:32 -0400

    Implement tra=cking history.

--- src/hooks/useVisualMode.js
+++ src/hooks/useVisualMode.js
@@ -4,11 +4,17 @@ import { useState } from "react";
 export default function useVisualMode(initialMode) {
 
   const [mode, setMode] = useState(initialMode);
+  const [history, setHistory] =  useState(initialMode);
+
   const transition = (newMode) => {
     setMode(newMode);
-  }
+    setHistory(prevHistory => [...prevHistory, newMode]);
+  };
+  const back = () => {
+
+  };
 
   return (
-    { mode, transition }
+    { mode, transition, back}
   );
 };
