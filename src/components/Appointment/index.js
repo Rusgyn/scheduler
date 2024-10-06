fec4d45e141e95840a3e5515f5e2fb1df2709a97
rusgyn <rusgynmagsanay@gmail.com>
2024-10-06 10:19:11 -0400

    Implement the functionality and pass the cancel props to Form.

--- src/components/Appointment/index.js
+++ src/components/Appointment/index.js
@@ -47,6 +47,7 @@ export default function Appointment(props) {
       {mode === CREATE && (
         <Form 
           interviewers = {props.interviewers}
+          onCancel = {() => back(EMPTY)}
         />
       )}
     </article>
