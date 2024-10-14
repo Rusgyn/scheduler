57c3bee5dfd19cd461448986274a72ed46e46c9d
rusgyn <rusgynmagsanay@gmail.com>
2024-10-14 15:49:45 -0400

    Integration Testing using mock modules.

--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,28 @@
 import React from "react";
 
-import { render, cleanup } from "@testing-library/react";
+import { render, cleanup, fireEvent } from "@testing-library/react";
 
 import Application from "components/Application";
 
 afterEach(cleanup);
 
-it("renders without crashing", () => {
-  render(<Application />);
+//Option 1: This is raw Promise syntax
+// it("defaults to Monday and changes the schedule when a new day is selected", () => {
+//   const { findByText, getByText, queryByText } = render(<Application />);
+//   //findByText return a promise.
+//   return findByText("Monday")
+//     .then(() => {
+//       fireEvent.click(getByText("Tuesday"));
+//       expect(queryByText("Leopold Silvers")).toBeInTheDocument();
+//     });
+// });
+
+//Option 2: using latest ECMASCRIPT2017, async/await promise
+it("defaults to Monday and changes the schedule when a new day is selected", async () => {
+  const { queryByText, getByText,findByText } = render(<Application />);
+
+  await findByText("Monday");
+
+  fireEvent.click(getByText("Tuesday"));
+  expect(queryByText("Leopold Silvers")).toBeInTheDocument();
 });
