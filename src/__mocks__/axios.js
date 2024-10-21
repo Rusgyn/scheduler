74b204d3f51f0cfedbb61957f9cf6f38a992f36f
rusgyn <rusgynmagsanay@gmail.com>
2024-10-21 17:49:09 -0400

    Add test that checks the functionality of Editing the appointment.

--- src/__mocks__/axios.js
+++ src/__mocks__/axios.js
@@ -53,6 +53,68 @@ const fixtures = {
   },
 };
 
+
+const resetFixtures = () => {
+  // Reset days
+  fixtures.days = [
+    {
+      id: 1,
+      name: "Monday",
+      appointments: [1, 2],
+      interviewers: [1, 2],
+      spots: 1,
+    },
+    {
+      id: 2,
+      name: "Tuesday",
+      appointments: [3, 4],
+      interviewers: [3, 4],
+      spots: 1,
+    },
+  ];
+
+  // Reset appointments
+  fixtures.appointments = {
+    1: { id: 1, time: "12pm", interview: null },
+    2: {
+      id: 2,
+      time: "1pm",
+      interview: { student: "Archie Cohen", interviewer: 2 },
+    },
+    3: {
+      id: 3,
+      time: "2pm",
+      interview: { student: "Leopold Silvers", interviewer: 4 },
+    },
+    4: { id: 4, time: "3pm", interview: null },
+  };
+
+  // Reset interviewers
+  fixtures.interviewers = {
+    1: {
+      id: 1,
+      name: "Sylvia Palmer",
+      avatar: "https://i.imgur.com/LpaY82x.png",
+    },
+    2: {
+      id: 2,
+      name: "Tori Malcolm",
+      avatar: "https://i.imgur.com/Nmx0Qxo.png",
+    },
+    3: {
+      id: 3,
+      name: "Mildred Nazir",
+      avatar: "https://i.imgur.com/T2WwVfS.png",
+    },
+    4: {
+      id: 4,
+      name: "Cohana Roy",
+      avatar: "https://i.imgur.com/FK8V841.jpg",
+    },
+  };
+};
+
+
 // USING IF STATEMENT. FOLLOWING COMPASS.
 export default {
   defaults: { baseURL: "" },//Add this line if error is TypeError: Cannot set property 'baseURL' of undefined
@@ -153,4 +215,5 @@ export default {
     });
   }),
   
+  resetFixtures,
 };
--- src/components/__tests__/Application.test.js
+++ src/components/__tests__/Application.test.js
@@ -1,11 +1,15 @@
 import React from "react";
+
 import { render, cleanup, fireEvent, prettyDOM, findByText, getAllByTestId, getByPlaceholderText, getByAltText, getByText, queryByText, findByAltText, findByTestId, queryByAltText, waitFor } from "@testing-library/react";
+
+import axios from "axios";
+import axiosMock from "__mocks__/axios"
 import Application from "components/Application";
 
 afterEach(cleanup);
 
 describe("Application", () => {
-  
+
   //Option 1: This is raw Promise syntax
   // it("defaults to Monday and changes the schedule when a new day is selected", () => {
   //   const { findByText, getByText, queryByText } = render(<Application />);
@@ -28,12 +32,11 @@ describe("Application", () => {
   });
 
   it("loads data, books an interview and reduces the spots remaining for Monday by 1", async () => {
-    const { container, debug } = render(<Application />);
-   
     // 1. Render the Application.
-    await findByText(container, "Archie Cohen");
-
+    const { container, debug } = render(<Application />);
     // 2. Wait until the text "Archie Cohen" is displayed.
+    await findByText(container, "Archie Cohen");
+    
     const appointments = getAllByTestId(container, "appointment");//Appointment component.index.js. data-testid="appointment". Output: Array.
     const appointment = appointments[0];
     
@@ -62,7 +65,7 @@ describe("Application", () => {
     const day = getAllByTestId(container, "day").find((day) => queryByText(day, "Monday"));//DayListItem component
     expect(getByText(day, "no spots remaining")).toBeInTheDocument();
   });
-
+  
   it("loads data, cancels an interview and increases the spots remaining for Monday by 1", async () => {
     // 1. Render the Application.
     const { container, debug } = render(<Application />);
@@ -71,9 +74,13 @@ describe("Application", () => {
     await findByText(container, "Archie Cohen");
   
     // 3. Click the "Delete" button on the booked appointment.
-    const appointment = getAllByTestId(container, "appointment").find((appointment) =>
+    const appointments = getAllByTestId(container, "appointment");
+    const appointment = appointments.find((appointment) =>
       queryByText(appointment, "Archie Cohen")
     );
+    // const appointment = getAllByTestId(container, "appointment").find((appointment) =>
+    //   queryByText(appointment, "Archie Cohen")
+    // );
   
     fireEvent.click(queryByAltText(appointment, "Delete"));
 
@@ -87,12 +94,55 @@ describe("Application", () => {
     expect(getByText(appointment, "Deleting...")).toBeInTheDocument();
   
     // 7. Wait until the element with the "Add" button is displayed.
-    const addButton = await findByTestId(container, "add-button"); // Should work now
+    const addButton = await findByTestId(container, "add-button");
     expect(addButton).toBeInTheDocument();
   
     // 8. Check that the DayListItem with the text "Monday" also has the text "2 spots remaining".
     const day = getAllByTestId(container, "day").find((day) => queryByText(day, "Monday"));
     expect(getByText(day, "2 spots remaining")).toBeInTheDocument();
   });
+
+  it("loads data, edits an interview and keeps the spots remaining for Monday the same", async () => {
+    axiosMock.resetFixtures();
+    // 1. Render the Application.
+    const { container, debug } = render(<Application />);
+   
+    // 2. Wait until the text "Archie Cohen" is displayed.
+    await findByText(container, "Archie Cohen");
+    expect(queryByText(container, "Archie Cohen")).toBeInTheDocument();
+
+    // 3. Click the "Edit" button on the booked appointment.
+    const appointment = getAllByTestId(container,"appointment").find((appointment) => queryByText(appointment,"Archie Cohen"));
+
+    fireEvent.click(getByAltText(appointment, "Edit"));
+
+    // 4. Enter the name "Lydia Miller-Jones" into the input with the placeholder "Enter Student Name"
+     fireEvent.change(getByPlaceholderText(appointment, /enter student name/i), {
+      target: { value: "Lydia Miller-Jones" },
+    });
+
+    // 5. Click the first interviewer in the list.
+    fireEvent.click(getByAltText(appointment, "Sylvia Palmer"));
+  
+    // 6. Click the "Save" button on that same appointment.
+    fireEvent.click(getByText(appointment, "Save"));
+
+    // 7. Check that the element with the text "Saving" is displayed.
+    expect(getByText(appointment, "Saving...")).toBeInTheDocument();
+
+    // 8. Wait until the element with the text "Lydia Miller-Jones" is displayed.
+    await findByText(appointment, "Lydia Miller-Jones");
+        
+    // 9. Check that the DayListItem with the text "Monday" also has the text "1 spot remaining".
+    const day = getAllByTestId(container, "day").find((day) => queryByText(day, "Monday"));
+
+    expect(getByText(day, "1 spot remaining")). toBeInTheDocument();
+  });
+
+  /* test number five */
+  it("shows the save error when failing to save an appointment", () => {
+    axios.put.mockRejectedValueOnce();
+  });
+
 
 });
